a592d1c9d98dfc90567913518539c19e5144f38b
Don Han <don_han@berkeley.edu>
2015-09-28 13:56:13 -0700

    google webmaster requirements' '

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,6 +29,11 @@ DEFAULT_PAGINATION = 10
 STATIC_PATHS = ['images', 'extra']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},
                        'extra/Resume_Don_Han.pdf':{'path': 'Resume_Don_Han.pdf'},
-                       'extra/favicon.ico':{'path': 'favicon.ico'},}
+                       'extra/favicon.ico':{'path': 'favicon.ico'},
+                       'extra/googlef7d891041bb9c9a5.html':{'path':'googlef7d891041bb9c9a5.html'},
+                       'extra/sitemap.xml':{'path':'sitemap.xml'}, }
+
+READERS = {'html':None}
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
