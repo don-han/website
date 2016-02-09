b8358138912d8157e9477e369c0e32f6bffe9332
Don Han <don_han@berkeley.edu>
2016-02-09 10:35:38 -0800

    using helpel.sh will automatically write article to draft folder with draft subtag. Not using DEFAULT_META because it's bothersome. fixes #4

--- helpel.sh
+++ helpel.sh
@@ -18,11 +18,17 @@ INPUTDIR=$BASEDIR/content
 
 ARTDIR=$INPUTDIR/articles
 PAGESDIR=$INPUTDIR/pages
+DRAFTDIR=$INPUTDIR/draft
 
 TODATE=$(date +'%Y-%m-%d')
 SLUG=$(echo $NAME | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z)
 EXT=md
-FILE_NAME="${TODATE}_${SLUG}"
+FILE_NAME="${TODATE}_${SLUG}.$EXT"
+
+ARTFILE=$ARTDIR/$FILE_NAME
+PAGESFILE=$PAGESDIR/$FILE_NAME
+DRAFTFILE=$DRAFTDIR/$FILE_NAME
+
 
 if [[ -z $PRIMARY_TAG ]]; then
     POST_NAME=${NAME}
@@ -33,19 +39,21 @@ fi
 
 
 if [[ $CMD == "newpost" ]]; then
-    echo "Title: $POST_NAME" >  $ARTDIR/$FILE_NAME.$EXT
-    echo "Slug: $SLUG" >> $ARTDIR/$FILE_NAME.$EXT
-    echo "Date: $TODATE" >> $ARTDIR/$FILE_NAME.$EXT
-    echo ""              >> $ARTDIR/$FILE_NAME.$EXT
-    echo ""              >> $ARTDIR/$FILE_NAME.$EXT
+    echo "Title: $POST_NAME" >  $DRAFTFILE
+    echo "Slug: $SLUG" >> $DRAFTFILE
+    echo "Date: $TODATE" >> $DRAFTFILE
+    echo "status: draft" >> $DRAFTFILE
+    echo ""              >> $DRAFTFILE
+    echo ""              >> $DRAFTFILE
 fi
 
 
 if [[ $CMD == "newpage" ]]; then
-    echo "Title: $POST_NAME" >  $PAGESDIR/$FILE_NAME.$EXT
-    echo "Slug: $SLUG" >> $PAGESDIR/$FILE_NAME.$EXT
-    echo ""              >> $PAGESDIR/$FILE_NAME.$EXT
-    echo ""              >> $PAGESDIR/$FILE_NAME.$EXT
+    echo "Title: $POST_NAME" >  $PAGESDIR/$FILE_NAME
+    echo "Slug: $SLUG" >> $PAGESDIR/$FILE_NAME
+    echo "status: draft" >> $PAGEDIR/$FILE_NAME
+    echo ""              >> $PAGESDIR/$FILE_NAME
+    echo ""              >> $PAGESDIR/$FILE_NAME
 fi
 
 
